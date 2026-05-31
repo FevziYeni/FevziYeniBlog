cbb748bfe89baac25a86dde214ec86489bf0c0ad
FevziYeni <fevziyeniiii@gmail.com>
2026-05-31 14:42:29 +0300

    Ana sayfaya landing bolumlerini ekle

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,10 @@
 import HeroSection from '../sections/HeroSection';
+import AboutSection from '../sections/AboutSection';
+import BlogSection from '../sections/BlogSection';
+import ContactSection from '../sections/ContactSection';
 import ProcessSection from '../sections/ProcessSection';
+import ProjectsSection from '../sections/ProjectsSection';
+import ServicesSection from '../sections/ServicesSection';
 
 type HomePageProps = {
   whatsappHref: string;
@@ -9,7 +14,12 @@ export default function HomePage({ whatsappHref }: HomePageProps) {
   return (
     <>
       <HeroSection whatsappHref={whatsappHref} />
+      <AboutSection />
+      <ServicesSection />
       <ProcessSection />
+      <ProjectsSection />
+      <BlogSection />
+      <ContactSection whatsappHref={whatsappHref} />
     </>
   );
 }
